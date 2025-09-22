d489c78581f376567b23cc6ff980557de56f06ba
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-09-22 10:05:01 +0200

    stop basedpyright

--- .lazy.lua
+++ .lazy.lua
@@ -6,4 +6,18 @@ vim.lsp.config("param-lsp", {
 
 vim.lsp.enable("param-lsp")
 
+vim.diagnostic.config({
+	virtual_text = { severity = { min = vim.diagnostic.severity.INFO } },
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	pattern = "*",
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client and client.name == "basedpyright" then
+			vim.lsp.stop_client(client.id)
+		end
+	end,
+})
+
 return {}
