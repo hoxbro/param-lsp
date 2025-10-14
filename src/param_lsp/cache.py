e0186246e0d495cd785d87b632f581e6efd0d6f2
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-10-14 11:20:16 +0200

    bump cache version

--- src/param_lsp/cache.py
+++ src/param_lsp/cache.py
@@ -19,7 +19,7 @@ from .models import ParameterInfo, ParameterizedInfo
 logger = get_logger(__name__, "cache")
 
 # Current cache version
-CACHE_VERSION = (1, 1, 0)
+CACHE_VERSION = (1, 1, 1)
 _re_no = re.compile(r"\d+")
 
 
